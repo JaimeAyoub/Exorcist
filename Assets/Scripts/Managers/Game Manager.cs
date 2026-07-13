a125e716374ddc03b43e5ef324b8424bd8f78a3f
JaimeAyoub <ayoubramosjaime@gmail.com>
2026-07-12 23:02:47 -0600

    Cosas del shader dithering

--- Assets/Scripts/Managers/Game Manager.cs
+++ Assets/Scripts/Managers/Game Manager.cs
@@ -1,11 +1,14 @@
 
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityUtils;
 
 public class GameManager : Singleton<GameManager>
 {
 
+    public Material DitherMat;
     public void EnableCursor()
     {
         Cursor.visible = true;
@@ -25,6 +28,18 @@ public class GameManager : Singleton<GameManager>
     public void SetTimeScale(float timeScale)
     {
         Time.timeScale = timeScale;
+    }
+
+    public void StartDither()
+    {
+        if(DitherMat != null)
+            DitherMat.DOFloat(0.01f,"_ColorResolution",0.5f).SetUpdate(true);
+    }
+
+    public void ResetDitherToDefault()
+    {
+        if(DitherMat != null)
+            DitherMat.DOFloat(16.0f,"_ColorResolution",1.0f).SetUpdate(true);
     }
 
 
